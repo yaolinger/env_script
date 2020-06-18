8af7e4e5a78d2fa897459b8de24a75ba999f5219
zhaojiaxun <1127406486@qq.com>
2020-06-18 16:41:47 +0800

    [CODE]修改资源拉取脚本

--- res/pull_res.sh
+++ res/pull_res.sh
@@ -3,9 +3,14 @@
 # IntegrationServer res
 echo "Pull integration server res.........."
 
+# 进入脚本所在目录
+cd `dirname $0`
+
 # 获取boost 1.67.0版本
 wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
 
 # 获取protobuf-master
 wget https://github.com/yaolinger/protobuf/archive/master.zip
 mv master.zip protobuf-master.zip
+
+cd -
